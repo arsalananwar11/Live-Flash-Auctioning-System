974c2f75bf2ab7e61bfd60beebcf98176df8df79
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-22 20:21:59 -0400

    Initial App Setup

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configuration from environment variables
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    
+    # Register blueprints for controllers
+    from app.controllers.main_controller import main
+    app.register_blueprint(main)
+    
+    return app
---
+++ app/services/main_service.py
@@ -0,0 +1,6 @@
+class MainService:
+    @staticmethod
+    def get_message(message: str=None):
+        if message:
+            return str(message)
+        return "Welcome to the Live Flash Auctioning System"
