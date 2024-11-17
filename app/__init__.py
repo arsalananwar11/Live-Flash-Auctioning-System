3d741e52ae4edff7934e115f12cc28959d6fe1fd
Affan Khamse <khamseaffan@gmail.com>
2024-11-17 18:47:50 -0500

    Flake updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from app.controllers import login_controller, main_controller, auction_controller
 from dotenv import load_dotenv
 import os
