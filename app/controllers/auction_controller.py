99f963136d9e573cea921e8e1af42f3f9aaf2923
Aditya Mittal <theadityamittal@gmail.com>
2024-11-17 19:13:31 -0500

    Add auction details page and leaderboard functionality

--- app/controllers/auction_controller.py
+++ app/controllers/auction_controller.py
@@ -4,10 +4,14 @@ from flask_login import login_required, current_user
 
 auction_controller = Blueprint("auction_controller", __name__)
 
-
 @auction_controller.route("/auctions")
 @login_required
 def auctions():
     message = MainService().get_message()
     message = f"{message} {current_user.name}"
     return render_template("index.html", message=message)
+
+@auction_controller.route("/auction_details/<int:auction_id>")
+def auction_details(auction_id):
+    print(f"Auction details for auction ID: {auction_id}")
+    return render_template("auctiondetails.html")
