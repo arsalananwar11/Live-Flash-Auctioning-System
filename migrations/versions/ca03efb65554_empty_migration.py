7dd285cca9f0802ec07d29cc6beaa1f4215d09a8
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-11-17 22:41:46 -0500

    Added DB migration files

---
+++ migrations/versions/ca03efb65554_empty_migration.py
@@ -0,0 +1,25 @@
+"""Empty migration
+
+Revision ID: ca03efb65554
+Revises: 
+Create Date: 2024-11-17 18:42:34.858494
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "ca03efb65554"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
