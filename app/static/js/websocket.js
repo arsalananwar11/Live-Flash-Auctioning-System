c3deb36e3711b9f2960cde860533e92b64d93926
Aditya Mittal <theadityamittal@gmail.com>
2024-12-06 18:57:54 -0500

    Leaderboard updates

--- app/static/js/websocket.js
+++ app/static/js/websocket.js
@@ -1,8 +1,10 @@
 export class AuctionWebSocket {
-  constructor(websocketUrl, auctionId, userId) {
+  constructor(websocketUrl, auctionId, userId, is_active, userName) {
     this.websocketUrl = websocketUrl;
     this.auctionId = auctionId;
     this.userId = userId;
+    this.userName = userName;
+    this.is_active = is_active;
     this.socket = null;
     this.connectionId = null; // Store the connection ID received from the server
   }
@@ -62,13 +64,27 @@ export class AuctionWebSocket {
     const tableBody = document.querySelector(".leaderboard-table tbody");
     tableBody.innerHTML = ""; // Clear current leaderboard
 
+    console.log("Leaderboard updated:", leaderboard);
+
+    if (leaderboard.length === 0) {
+      const row = document.createElement("tr");
+      row.classList.add("no-bids-row");
+      row.innerHTML = `
+        <td colspan="4" class="no-bids-message">
+          <div class="cell-content">No bids placed yet</div>
+        </td>
+      `;
+      tableBody.appendChild(row);
+      return;
+    }
+
     leaderboard.forEach((entry, index) => {
       const row = document.createElement("tr");
       row.classList.add("table-row");
 
       row.innerHTML = `
         <td class="table-cell"><div class="cell-content">${index + 1}</div></td>
-        <td class="table-cell"><div class="cell-content">${entry.username}</div></td>
+        <td class="table-cell"><div class="cell-content">${entry.user_name}</div></td>
         <td class="table-cell"><div class="cell-content">$${entry.bid_amount}</div></td>
         <td class="table-cell"><div class="cell-content">${new Date(
           entry.timestamp * 1000
@@ -76,6 +92,9 @@ export class AuctionWebSocket {
       `;
       tableBody.appendChild(row);
     });
+
+    const topBid = leaderboard[0].bid_amount;
+    document.querySelector(".bid-frame span:last-child").textContent = `$${topBid}`;
   }
 
   disconnectAndSendMessage() {
@@ -84,8 +103,6 @@ export class AuctionWebSocket {
         // Send the disconnect message with the connection ID
         this.sendMessage("disconnect", {
           connection_id: this.connectionId,
-          user_id: this.userId,
-          auction_id: this.auctionId,
         });
       } else {
         console.error("Connection ID is not available. Cannot send disconnect message.");
