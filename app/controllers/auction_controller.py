abe2b60bc5fbffdcff6e7f5629ced39862c14c8c
Aditya Mittal <theadityamittal@gmail.com>
2024-12-06 19:54:40 -0500

    code cleanup

--- app/controllers/auction_controller.py
+++ app/controllers/auction_controller.py
@@ -36,23 +36,14 @@ def auction_details(auction_id):
         # Simulate a user ID for testing purposes
         user_id = current_user.id
 
-        # Convert start_time and end_time to datetime object
         start_time_str = target_auction.get("start_time")
-        # end_time_str = target_auction.get("end_time")
         start_time = None
-        # end_time = None
         if start_time_str:
             try:
                 start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M:%S")
             except ValueError as e:
                 # Log the error and keep `start_time` as None
                 print(f"Error parsing start_time: {e}")
-        # if end_time_str:
-        #     try:
-        #         end_time = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M:%S")
-        #     except ValueError as e:
-        #         # Log the error and keep `end_time` as None
-        #         print(f"Error parsing end_time: {e}")
 
         # Pass data to the frontend
         return render_template(
@@ -65,40 +56,6 @@ def auction_details(auction_id):
         )
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-# def auction_details(auction_id):
-#     # target_auction = AuctionService().get_auction(auction_id)
-
-#     # if target_auction is None:
-#     #     return "Auction not found", 404
-
-#     # return render_template("auction_details.html", auction=target_auction)
-#     try:
-#         # Fetch the target auction
-#         target_auction = AuctionService().get_target_auction(auction_id)
-
-#         if target_auction is None:
-#             return "Auction not found", 404
-
-#         # Fetch WebSocket URL from the environment
-#         websocket_url = os.getenv("WEB_SOCKET_URL")
-#         if not websocket_url:
-#             return "WebSocket URL not configured", 500
-
-#         # Simulate a user ID for testing purposes
-#         user_id = current_user.id
-
-#         # Pass data to the frontend
-#         return render_template(
-#             "auction_details.html",
-#             auction=target_auction,
-#             websocket_url=websocket_url,
-#             user_id=user_id,
-#             auction_id=auction_id,
-#         )
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
 
 
 @auction_controller.route("/create")
