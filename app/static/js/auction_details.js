be9704b1e1745e549e5b3f18fb682126aa097e4e
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-12-09 19:48:47 -0500

    Auction dashboard UI improvements

--- app/static/js/auction_details.js
+++ app/static/js/auction_details.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <span>${increment}%</span>
       `;
       button.addEventListener("click", function () {
-        const currentBid = document.querySelector(".bid-frame span:last-child").textContent;
+        const currentBid = document.getElementById("top-bid").textContent;
         const newBid = (
           Number(currentBid.replace(/[^\d.-]/g, "")) * (1 + increment / 100)
         ).toFixed(2);
@@ -36,35 +36,4 @@ document.addEventListener("DOMContentLoaded", function () {
       });
       bidButtonsDiv.appendChild(button);
     });
-  
-    // Create custom bid button and form
-    // const customBidButton = document.createElement("button");
-    // customBidButton.classList.add("bid-button");
-    // customBidButton.innerHTML = `<img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b3839e35b1bccdc5aaeccb24adf1904e8555b71edb26def35edde63ef1057946?placeholderIfAbsent=true&apiKey=ef34feccb14a4349a0b21d81082028c5" alt="" class="button-icon" /><span>Custom</span>`;
-    // bidButtonsDiv.appendChild(customBidButton);
-
-    // const customBidForm = document.createElement("form");
-    // customBidForm.classList.add("custom-input");
-    // customBidForm.innerHTML = `
-    //     <label for="customAmount" class="visually-hidden">Custom Amount</label>
-    //     <div class="input-content" aria-label="Custom Amount">
-    //         <span class="dollar-sign">$</span>
-    //         <input type="number" id="customAmount" placeholder="Custom Amount" style="border: none;" />
-    //     </div>
-    // `;
-    // bidButtonsDiv.appendChild(customBidForm);
-  
-    // Handle custom bid submission
-    // customBidButton.addEventListener("click", function () {
-    //   const customAmount = document.getElementById("customAmount").value;
-    //   if (customAmount) {
-    //     const newBid = parseFloat(customAmount).toFixed(2);
-    //     document.querySelector(".bid-frame span:last-child").textContent = `$${newBid}`;
-  
-    //     // Send the custom bid through WebSocket
-    //     placeBid(newBid);
-    //   } else {
-    //     console.error("Custom bid amount is empty.");
-    //   }
-    // });
   });
