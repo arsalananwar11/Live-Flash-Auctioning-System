c6009439dc4f0a0e7d22cdb6c4f935a11900fd25
Affan Khamse <khamseaffan@gmail.com>
2024-12-09 21:46:30 -0500

    Resloved time issue

--- lambda_functions/StartAuctionLambda.py
+++ lambda_functions/StartAuctionLambda.py
@@ -5,15 +5,18 @@ from datetime import datetime
 import os
 
 dynamodb = boto3.resource("dynamodb")
-eventbridge_client = boto3.client('events')
+eventbridge_client = boto3.client("events")
 auction_table = dynamodb.Table("auction-connections")
-apigateway_management_api = boto3.client("apigatewaymanagementapi", endpoint_url=os.environ["WEBSOCKET_ENDPOINT"])
+apigateway_management_api = boto3.client(
+    "apigatewaymanagementapi", endpoint_url=os.environ["WEBSOCKET_ENDPOINT"]
+)
 
 rds_host = os.environ["DB_HOSTNAME"]
 rds_port = int(os.environ["DB_PORT"])
 rds_db_name = os.environ["DB_NAME"]
 rds_user = os.environ["DB_USERNAME"]
 rds_password = os.environ["DB_PASSWORD"]
+
 
 def connect_to_rds():
     try:
@@ -29,6 +32,7 @@ def connect_to_rds():
     except Exception as e:
         print(f"Error connecting to RDS: {str(e)}")
         raise e
+
 
 def update_rds(auction_id, is_active):
     try:
@@ -51,10 +55,11 @@ def update_rds(auction_id, is_active):
         if connection:
             connection.close()
 
+
 def delete_eventbridge_rule(rule_name):
     try:
         targets_response = eventbridge_client.list_targets_by_rule(Rule=rule_name)
-        target_ids = [target['Id'] for target in targets_response.get('Targets', [])]
+        target_ids = [target["Id"] for target in targets_response.get("Targets", [])]
 
         if target_ids:
             eventbridge_client.remove_targets(Rule=rule_name, Ids=target_ids)
@@ -67,7 +72,8 @@ def delete_eventbridge_rule(rule_name):
     except Exception as e:
         print(f"Error deleting rule {rule_name}: {str(e)}")
         raise
-    
+
+
 def send_websocket_message(connection_id, message):
     try:
         apigateway_management_api.post_to_connection(
@@ -78,6 +84,7 @@ def send_websocket_message(connection_id, message):
     except Exception as e:
         print(f"Error sending message: {e}")
 
+
 def lambda_handler(event, context):
     print(f"Received event: {json.dumps(event)}")
     auction_id = event["auction_id"]
@@ -86,19 +93,17 @@ def lambda_handler(event, context):
     auction_table.update_item(
         Key={"auction_id": auction_id},
         UpdateExpression="SET auction_status = :status",
-        ExpressionAttributeValues={":status": "IN_PROGRESS"}
+        ExpressionAttributeValues={":status": "IN_PROGRESS"},
     )
-    
+
     send_websocket_message(auction_connection_id, message)
 
     # Update RDS
     connection = connect_to_rds()
     with connection.cursor() as cursor:
         cursor.execute(
-            "UPDATE auction SET is_active = 1 WHERE auction_id = %s",
-            (auction_id,)
+            "UPDATE auction SET is_active = 1 WHERE auction_id = %s", (auction_id,)
         )
         connection.commit()
 
-    
     delete_eventbridge_rule(f"StartAuction_{auction_id}")
